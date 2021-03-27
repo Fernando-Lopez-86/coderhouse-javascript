924b199e725dacc336d48c1fe0bcebd05e66513c
Ramiro Nicolas D'Accorso Rosati <ramiro.daccorso@gmail.com>
2021-03-27 10:14:58 -0300

    Correcciones
    Muy bien Fer, ahi te creo un pull request con unas mejoras. Primero fijate que remplace los alerts para que contengan string literales en lugar de strings comunes, que entre otras mejoras no necesitan concatenar variables con mas. Te invito a investigar el tema en https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Template_literals 
    Y por ultimo te agrege los punto y comafinales. Recorda usarlos siempre. No es obligacion ya que el lenguaje no lo requiere, motivo por el cual no te lovamos a exigir, sin embargo es una muy buena practica.

--- desafio1/index.js
+++ desafio1/index.js
@@ -1,12 +1,14 @@
-let numero = prompt('Ingrese un numero')
-numero = parseInt(numero)
+let numero = prompt('Ingrese un numero');
+numero = parseInt(numero);
 
 if(numero){
     if(numero % 2 === 0){
-        alert('El numero ' + numero + ' es par')
+        //alert('El numero ' + numero + ' es par');
+        alert(`El numero ${numero} es par`);
     }else{
-        alert('El numero ' + numero + ' es impar')
+        //alert('El numero ' + numero + ' es impar');
+        alert(`El numero ${numero} es inpar`);
     }
 }else{
-    alert('Ingrese un valor correcto')
+    alert('Ingrese un valor correcto');
 }
