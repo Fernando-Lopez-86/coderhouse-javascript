2fb4fe62609ffd4608731276eaa6b6c5d992b16c
Fernando <ferdinandolopez@hotmail.com>
2021-04-03 18:42:48 -0300

    desafio2

---
+++ desafio2/js/functions.js
@@ -0,0 +1,18 @@
+
+function mostrarMensaje() {
+
+    let tipo = document.getElementById("tipo").value;
+    let pesos = document.getElementById("pesos").value;
+    let resultado;
+
+    switch (tipo) {
+        case "dolar":
+            let resultadodolar = pesos * 141;
+            return alert("Cotizacion en dolares: " + resultadodolar);
+            break;
+        case "euro":
+            let resultadoeuro = pesos * 176;
+            return alert("Cotizacion en euros: " + resultadoeuro);
+            break;
+    }
+}
